89526c41d932b95b0af22527b0fdb07f6db3f07c
YakiroYuri <yurilealdacruz@gmail.com>
2024-03-19 13:00:25 -0300

    adicionando o restante das salas

--- estoque/polls/models.py
+++ estoque/polls/models.py
@@ -62,6 +62,21 @@ SALA_CHOICES = [
         ('SALA 4.1.6', 'SALA 4.1.6'),
         ('SALA 4.1.7', 'SALA 4.1.7'),
         ('SALA 4.1.8', 'SALA 4.1.8'),
+        ('SALA 5.4.1','SALA 5.4.1')
+        ('SALA 5.4.2','SALA 5.4.2')
+        ('SALA 5.4.3','SALA 5.4.3')
+        ('SALA 5.4.4','SALA 5.4.4')
+        ('SALA 5.4.5','SALA 5.4.5')
+        ('SALA 5.4.6','SALA 5.4.6')
+        ('SALA 5.4.7','SALA 5.4.7')
+        ('SALA 5.4.8','SALA 5.4.8')
+        ('SALA 5.4.9','SALA 5.4.9')
+        ('SALA 5.4.10','SALA 5.4.10')
+        ('SALA 5.4.11','SALA 5.4.11')
+        ('SALA 5.4.12','SALA 5.4.12')
+        ('SALA 5.4.13','SALA 5.4.12')
+        ('SALA 5.4.14','SALA 5.4.14')
+        ('SALA 5.4.15','SALA 5.4.15')
 ]
 
 
